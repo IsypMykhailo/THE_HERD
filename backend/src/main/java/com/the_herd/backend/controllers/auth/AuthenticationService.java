cad211a3626d2671255d4f03e4a8a3badfc02b06
IsypMykhailo <amazonkanameforme@gmail.com>
2024-03-05 21:16:30 -0800

    authentication and security

---
+++ backend/src/main/java/com/the_herd/backend/controllers/auth/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.the_herd.backend.controllers.auth;
+
+import com.the_herd.backend.config.JwtService;
+import com.the_herd.backend.models.UserRepository;
+import com.the_herd.backend.models.user.Role;
+import com.the_herd.backend.models.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/the_herd/backend/models/user/Role.java
@@ -0,0 +1,6 @@
+package com.the_herd.backend.models.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
