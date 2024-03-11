46a4faffac1268ae1d7710db4d484d59a85ac202
Yazeed69 <yazeedf73@icloud.com>
2024-03-10 19:38:57 -0700

    Implemented authentication form

--- backend/src/main/java/com/the_herd/backend/controllers/auth/AuthenticationController.java
+++ backend/src/main/java/com/the_herd/backend/controllers/auth/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.the_herd.backend.controllers.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthenticationController {
     private final AuthenticationService service;
 
@@ -24,6 +26,7 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody AuthenticationRequest request
     ) {
+        System.out.println("Auth");
         return ResponseEntity.ok(service.authenticate(request));
     }
 }
