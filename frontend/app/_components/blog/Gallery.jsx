7d6f5b8be5e5d3d6728f49db42f4ff760789002a
IsypMykhailo <amazonkanameforme@gmail.com>
2024-03-27 01:20:35 -0700

    blog page smooth scroll

--- frontend/app/_components/blog/Gallery.jsx
+++ frontend/app/_components/blog/Gallery.jsx
@@ -4,14 +4,13 @@ import "../../_css/Blog.css"
 import {useState, useEffect, useRef, Children, useLayoutEffect} from "react";
 import Image from "next/image";
 import {useLenis} from "@studio-freight/react-lenis";
-import gsap from 'gsap'
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 // TODO: Fix the Gallery to allow correct horizontal scrolling
 const Gallery = () => {
     const [gallery, setGallery] = useState([]);
-    // const galleryRef = useRef(null)
-    // const timelineRef = useRef(null)
-    // const spacerRef = useRef(null)
+    const containerRef = useRef(null)
+    const galleryRef = useRef(null)
 
     useEffect(() => {
         fetch('/gallery.json')
@@ -23,38 +22,29 @@ const Gallery = () => {
             .catch((error) => console.error("Fetching blogs failed:", error));
     }, []);
 
-    // useEffect(() => {
-    //     if(!galleryRef.current) return
-    //
-    //     spacerRef.current.style.height = galleryRef.current.clientWidth
-    //
-    //     timelineRef.current = gsap.timeline({
-    //         scrollTrigger: {
-    //             trigger: galleryRef.current,
-    //             pin: false,
-    //             start: 'top top',
-    //             end: galleryRef.current.clientWidth,
-    //             scrub: 1
-    //         }
-    //     }).to(galleryRef.current, {
-    //         translateX: -galleryRef.current.clientWidth + window.innerWidth
-    //     })
-    //
-    //     console.log(galleryRef.current.offsetWidth, galleryRef.current.clientWidth, galleryRef.current.scrollWidth)
-    // }, [galleryRef]);
-    //
-    // useLayoutEffect(() => {
-    //     if(!galleryRef.current) return
-    //             console.log(galleryRef.current)
-    //     console.log(galleryRef.current.getBoundingClientRect())
-    // }, [galleryRef]);
+    useEffect(() => {
+        if (!galleryRef.current || !containerRef.current) return
+
+        ScrollTrigger.create({
+            trigger: containerRef.current,
+            start: 'top top',
+            end: `+=${containerRef.current.clientHeight}`,
+            onUpdate: self => {
+                galleryRef.current.scrollLeft = galleryRef.current.clientWidth * self.progress
+            }
+        })
+        return () => {
+            ScrollTrigger.killAll()
+        }
+    }, [galleryRef, containerRef]);
 
     return (
-        <div>
-            <div className={"gallery-container top-0 left-0 overflow-x-auto"}>
+        <div className='xl:h-[200vh] relative' ref={containerRef}>
+            <div ref={galleryRef} id={'carousel'} className={"gallery-container xl:fixed xl:top-0 xl:left-0 w-screen xl:h-screen"}>
                 {gallery.map((el, index) => (
                     el.type === 'image' ? (
-                        <Image key={index} src={el.src} alt={`Image ${el.id}`} width={0} height={0} className={"gallery-entity"}
+                        <Image key={index} src={el.src} alt={`Image ${el.id}`} width={0} height={0}
+                               className={"gallery-entity"}
                                unoptimized/>
                     ) : (
                         <video key={index} controls style={{width: '100%'}}>
