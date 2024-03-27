88315e07ed8daad0ac32b9556e7a12d8d1f9e9f6
IsypMykhailo <amazonkanameforme@gmail.com>
2024-03-26 21:53:57 -0700

    fixed log out bug

--- frontend/app/_components/Header.jsx
+++ frontend/app/_components/Header.jsx
@@ -28,7 +28,7 @@ const Header = ({setLoading}) => {
             setLoading(true);
             try {
                 const response = await fetch("https://the-herd.braverock-df19d8cb.eastus.azurecontainerapps.io/api/v1/auth/validateSession", {
-                    method: 'GET',
+                    method: 'POST',
                     credentials: 'include',
                 });
 
