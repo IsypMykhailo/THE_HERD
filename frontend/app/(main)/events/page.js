fe1d6f9a6d7f0c878eb9ee66e711aa08b840852e
chloe0521 <chloe.ng0521@gmail.com>
2024-04-01 13:24:18 -0700

    Edited fetch link + edited the Event.java file

--- frontend/app/(main)/events/page.js
+++ frontend/app/(main)/events/page.js
@@ -9,7 +9,7 @@ import {useEffect, useState} from "react";
 const Events = () => {
     const [events, setEvents] = useState([]);
     useEffect(() => {
-        fetch('/api/v1/events/get/all')
+        fetch('https://the-herd.braverock-df19d8cb.eastus.azurecontainerapps.io/api/v1/events/get/all')
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response failed');
