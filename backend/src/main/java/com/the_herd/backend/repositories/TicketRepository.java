c35dbb41889f85cda41cb446adbee65e955b4288
ArfaDhukka02 <ada144@sfu.ca>
2024-04-03 02:24:46 -0700

    UserRepo changes

--- backend/src/main/java/com/the_herd/backend/repositories/TicketRepository.java
+++ backend/src/main/java/com/the_herd/backend/repositories/TicketRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
+
 
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
-    Optional<Ticket> findByFirstNameAndLastName(String firstName, String lastName);
+    Optional<Ticket> findByUserId(UUID userId);
 }
