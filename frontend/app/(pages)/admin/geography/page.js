dec595319dee30191b6b4a3b2c07b61b6811e998
Aaron Zhou <126049003+Azpochita@users.noreply.github.com>
2024-04-06 15:30:57 -0700

    Revert "Merge pull request #27 from IsypMykhailo/admin-auth"
    This reverts commit 3523bf3d3dae955bfab4a329135e21c3ede74cc0, reversing
    changes made to d8e802ecdbc48a39ee40d8f4965aad5151640e10.

---
+++ frontend/app/(pages)/admin/geography/page.js
@@ -0,0 +1,26 @@
+'use client'
+
+import { Box, useTheme } from "@mui/material";
+import GeographyChart from "../../../_components/admin/GeographyChart";
+import Header from "../../../_components/admin/Header";
+import { tokens } from "../../../_components/admin/theme";
+
+const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="Geography" subtitle="Simple Geography Chart" />
+
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Geography;
