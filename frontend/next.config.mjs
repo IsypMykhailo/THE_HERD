302fb912c7926ca33764178be0eb3c1d0cc0b89d
IsypMykhailo <amazonkanameforme@gmail.com>
2024-05-02 12:20:09 -0700

    apiurl change to external

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        apiUrl: 'http://localhost:8080'
-        // apiUrl: 'https://the-herd.braverock-df19d8cb.eastus.azurecontainerapps.io'
+        // apiUrl: 'http://localhost:8080'
+        apiUrl: 'https://the-herd.braverock-df19d8cb.eastus.azurecontainerapps.io'
     }
 };
 
